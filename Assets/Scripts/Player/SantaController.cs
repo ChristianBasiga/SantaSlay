4c845ccb77681f5846b624ddc36701caaf73d56b
Prince Christian Basiga <princebasiga@gmail.com>
2017-12-18 20:12:14 -0800

    Added prototype GameObjects for Present and Coal prefabs for Ammo pool to copy from to reduce Disk I/O into resources

--- Assets/Scripts/Player/SantaController.cs
+++ Assets/Scripts/Player/SantaController.cs
@@ -10,11 +10,25 @@ public class SantaController : MonoBehaviour
     private Santa santa;
     //rigid component 
     private Rigidbody2D r2d;
+
+
+    public GameObject coalPrototype;
+    public GameObject presentPrototype;
+
     // Use this for initialization
+    ReuseablePool<SantaAmmo> bulletPool;
     void Start()
     {
         //get component
         r2d = GetComponent<Rigidbody2D>();
+
+        //Retrieves the prefabs for coal and present ammo
+        //These will act as prototypes to reduce Disk I/O and complicating Pool
+        coalPrototype = Resources.Load(string.Format("Prefabs/Ammo/{0}", GameConstants.SantaAmmoType.COAL.ToString()));
+        presentPrototype = Resources.Load(string.Format("Prefabs/Ammo/{0}", GameConstants.SantaAmmoType.PRESENT.ToString()));
+
+        //Starts off bullet buffer with 200
+        bulletPool = new ReuseablePool<SantaAmmo>(200);
         santa = new Santa(10, 0, 5);
     }
     void FixedUpdate()
@@ -32,5 +46,34 @@ public class SantaController : MonoBehaviour
 
         //movement 
         r2d.AddForce(movement * santa.Speed);
+
+
     }
+
+    void Update()
+    {
+
+        //Just k for testing, will be something else later
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            santa.switchAmmo();
+
+        }
+        //Else cause can't shoot and swap ammo at same time
+        else if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Shoot();
+        }
+
+    }
+
+
+    void Shoot()
+    {
+        
+
+
+    }
+
+
 }
