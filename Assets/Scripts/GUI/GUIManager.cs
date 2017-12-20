f6d4cefdf626887963953b97eeb245e5f16fbf0d
Prince Christian Basiga <princebasiga@gmail.com>
2017-12-20 01:13:51 -0800

    Added callbacks to update UI elements for points and health

--- Assets/Scripts/GUI/GUIManager.cs
+++ Assets/Scripts/GUI/GUIManager.cs
@@ -1,13 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GUIManager : MonoBehaviour {
 
+    public Slider santaHP;
+    public Text pointsLabel;
+    Santa santa;
+
+
+    void Awake()
+    {
+        santa = GameObject.FindGameObjectWithTag("Player").GetComponent<SantaController>().santa;
+    }
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+        //Todo: Test these to make sure call backs working right
+
+        santaHP.maxValue = santa.Health;
+
+        santa.healthUpdated += (int newHealth) => { santaHP.value = newHealth; };
+        santa.pointsUpdated += (int newPoints) => { pointsLabel.text = "Scores: " + newHealth.ToString(); };
+    }
 	
 	// Update is called once per frame
 	void Update () {
