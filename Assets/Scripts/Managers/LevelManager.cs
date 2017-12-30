3e8cf1d5af7c6cb7aab428db9d9662f7c852424d
Prince Christian Basiga <princebasiga@gmail.com>
2017-12-30 01:58:54 -0800

    Implmented methods for spawning all obstacles

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -13,23 +13,38 @@ namespace SantaGame {
 
         public Transform background;
         public Transform foreground;
-         PoolManager poolManager;
+        PoolManager poolManager;
 
         
         //GOtta think of better ways to form delegates after this. I shouldn't be making one for every single event.
-        //Though in this case there has been none with this signature of two arguments.
+        //Though in this case there has been none with this signature of two arguments
         public delegate void LevelProgressed(int progress, int goal);
 
+        #region Obstacle Variables
+
+        GameObject[] snowFlakeSpawnPoints;
+        Obstacle snowflakePrefab;
+
+        Obstacle starPrefab;
+        Obstacle wreethPrefab;
+
+
+        #endregion
+
+        #region House Related Varaibles
         //Same name event in House, but different purpose and signature, prob confusing but fuck for now.
         public event LevelProgressed PassedHouse;
 
         //I want to reuse the one in reusable sincree same signature
         int numHouses;
         int passedHouses;
+
         House housePrefab;
 
         //All possible spawn points
         private List<Vector3> houseSpawnPoints;
+
+    #endregion
 
         //Will pass in next level and current difficulty by calling callback as soon as current houses went through 0.
         //GUI manager needs to add it's own callback here to load the loading screen while next level getting set up.
@@ -54,10 +69,7 @@ namespace SantaGame {
         // Use this for initialization
         void Start()
         {
-            //How often should obstacles spawn?
-            //That's game design.
-            //To determine that, let's see how player experience plays out.
-            //Player going through level, dropping presents
+         
      
             SantaController santa = GameObject.Find("Santa").GetComponent<SantaController>();
 
@@ -70,12 +82,70 @@ namespace SantaGame {
             passedHouses = 0;
             numHouses = 0;
 
+
+            //Wait, wait. I only need obstacles pool cause that's these all are. Specific instnaces means they can act as prototypes.
+            //Simplicity sake, I'ma just have them ahve different pools, fuck it. Otherwise would need to copy everything, sprite, transform, etc. over
+            //Not that need seperate pools for them
+
+
+            //Cause all share same pool.
+            snowflakePrefab = ((GameObject)Resources.Load("Prefabs/Snowflake")).GetComponent<Obstacle>();
+            snowflakePrefab.ReuseID = 3;
+            starPrefab = ((GameObject)Resources.Load("Prefabs/Star")).GetComponent<Obstacle>();
+            starPrefab.ReuseID = 4;
+            wreethPrefab = ((GameObject)Resources.Load("Prefabs/Wreeth")).GetComponent<Obstacle>();
+            wreethPrefab.ReuseID = 5;
+
+            
+            
+            poolManager.AddPool(snowflakePrefab, 5);
+            //Very rare for there to be more than 1 star, tbh this is waste of pool for this, might as well just instantiate it
+            //cause it's not as bad as houses, and snowflakes etc. But will leave as is for now, just incase
+            poolManager.AddPool(starPrefab, 2);
+
+            poolManager.AddPool(wreethPrefab, 5);
+
             santa.BirdHit += () => { timeLeftMultiplier = multiplierTime; };
 
         }
 
-       
 
+       
+        void SpawnStar()
+        {
+            //ToDo: spawn store, and place in correct position
+            //To make "correct position" easier I could do initial plan of just having moving backgrounds
+            //instead of actually moving santa outside of up and down. and that would mean
+            //getting rid of speed factor(well can still use for up/down movement though) but yeah would make things
+            //ALOT easier of level itself was just translating. Also then wouldn't need to have position var for star, cause could just leave it at prefabs position meaning this:
+            //This is literally All I need to do, if I did that.
+            Reuseable star = poolManager.Acquire(starPrefab.ReuseID);
+            star.gameObject.SetActive(true);
+           
+        }
+
+        //Exactly the same situation with this, just start off screen then come in, holy fuck okay number one priority is get that working
+        void SpawnWreeths(int n)
+        {
+            Reuseable wreeth = poolManager.Acquire(wreethPrefab.ReuseID);
+            wreeth.gameObject.SetActive(true);
+            //Difference is there may be alot of them, so could make this Ienumerator so could add delay.
+        }
+
+        void SpawnSnowFlakes(int n)
+        {
+            //Need spawn points for snowflakes
+            for (int i = 0; i < snowFlakeSpawnPoints.Length; ++i)
+            {
+                Reusable snowflake = poolManager.Acquire(snowflakePrefab.ReuseID);
+
+                snowflake.gameObject.transform.position = snowFlakeSpawnPoints[i];
+                snowflake.gameObject.SetActive(true);
+            }
+
+        }
+
+        #region House related Methods
         public int NumberOfHouses
         {
             set
@@ -123,6 +193,7 @@ namespace SantaGame {
             }
         }
 
+        #endregion
 
         public float NaughtyChance
         {
--- Assets/Scripts/Obstacles/Obstacle.cs
+++ Assets/Scripts/Obstacles/Obstacle.cs
@@ -13,19 +13,19 @@ namespace SantaGame {
      * 
      
    */
-    public delegate Transform ObstacleMoveFunction(Transform intial,float speed, float pattSpeed);
+    public delegate void ObstacleAction(Transform intial,float speed, float pattSpeed);
 
     public class Obstacle : Reusable
     {
         //Really only needs to be in this scope, and it will be decided at start what it will have, based on the name of prefab
-        public static readonly Dictionary<string, ObstacleMoveFunction> ObstacleActions = new Dictionary<string, ObstacleMoveFunction>()
+        public static readonly Dictionary<string, ObstacleAction> ObstacleActions = new Dictionary<string, ObstacleAction>()
         {
             //Okay since very special movement, inheritance might be okay after all
             {"Snowflake", (Transform init, float movSpeed, float patternSpeed) => {
 
-                Vector2 movement = init.position + Vector2.up * -movSpeed;
+                Vector3 movement = init.position + Vector3.up * -movSpeed;
                 float amplitude = 0.8f;
-                Vector2 waveAxis = Vector2.right * Mathf.Sin(Time.time * patternSpeed ) * amplitude;
+                Vector3 waveAxis = Vector3.right * Mathf.Sin(Time.time * patternSpeed ) * amplitude;
 
                 init.position =  movement + waveAxis;
 
@@ -35,16 +35,17 @@ namespace SantaGame {
                 //Will just be diagonal movement, but not y = x.
 
                 //Tbh instead of returning since passing transform now, I could just straight up change the position too.
-                Vector2 direction = (Vector2.up * -patternSpeed) + (Vector2.right * -movSpeed);
+                Vector3 direction = (Vector3.up * -patternSpeed) + (Vector3.right * -movSpeed);
 
-                init.localScale += new Vector2(1,1) * patternSpeed;
+                init.localScale += new Vector3(1,1) * patternSpeed;
 
                 init.position += direction;
 
             } },
-            { "Wreeth" , (Transform init, float speed) => {
+            { "Wreeth" , (Transform init, float speed, float pattSpeed) => {
 
-                //Idk yet.
+                transform.position -= Vector3.right * speed;
+                //Not sure what to do with pattspeed yet
 
             } }
 
@@ -59,22 +60,21 @@ namespace SantaGame {
         public float speed;
         public float pattSpeed;
 
-        ObstacleMoveFunction move;
+        ObstacleAction action;
 
-        public void SetMovement(ObstacleMoveFunction move)
+        void Start()
         {
-            this.move = move;
+            action = ObstacleActions[gameObject.tag];
         }
+      
 
 
         void Update()
         {
-            if (move != null)
-                move(transform.position, Time.deltaTime * speed,pattSpeed * Time.deltaTime);
+            if (action != null)
+                action(transform, Time.deltaTime * speed,pattSpeed * Time.deltaTime);
         }
 
-
-        
         //This would be something all derived classes will have.
         void OnTriggerEnter2D(Collider2D other)
         {
